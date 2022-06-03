e2237ab5c984fcbeca241ef10628330b918f9035
Alexandr Timoshenko <mr.atimoshenko@gmail.com>
2022-06-03 15:32:03 -0700

    if index 0

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -3,7 +3,7 @@
 // Example 1 and 2
 var search = function(nums, target) {
     let found = nums.indexOf(target);
-    if (found){
+    if (found>=0){
         return alert (found);
     }
     else{
@@ -16,5 +16,6 @@ var search = function(nums, target) {
 search([-1,0,3,5,9,12], 9);
 // Example 2
 search([-1,0,3,5,9,12], 2);
-
+// Example 3
+search([5], 5);
 
