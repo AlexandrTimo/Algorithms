831fc241b845d018e137f95b6b1d579bcb0f08a3
Alexandr Timoshenko <mr.atimoshenko@gmail.com>
2022-06-08 18:00:32 -0700

    Added final value variable

---
+++ final_value_of_variable_after_performing_operations/final_value_of_variable_after_performing_operations.js
@@ -0,0 +1,24 @@
+// -- Final Value of Variable After Performing Operations --
+
+var finalValueOperations = function (operations) {
+  let x = 0;
+  operations.forEach((cur) => {
+    if (cur === "++X" || cur === "X++") {
+      x = x + 1;
+    } else if (cur === "--X" || cur === "X--") {
+      x = x - 1;
+    } else {
+      x = 0;
+    }
+  });
+  return x;
+};
+
+// Example 1:
+console.log(finalValueOperations(["--X", "X++", "X++"]));
+
+// Example 2:
+console.log(finalValueOperations(["++X", "++X", "X++"]));
+
+// Example 3:
+console.log(finalValueOperations(["X++", "++X", "--X", "X--"]));
