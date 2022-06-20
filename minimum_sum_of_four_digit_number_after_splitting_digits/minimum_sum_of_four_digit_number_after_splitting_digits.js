bd85d134a3cb9fa9fc31a7272a54074a8cf107df
Alexandr Timoshenko <mr.atimoshenko@gmail.com>
2022-06-19 22:51:26 -0700

    modified sim sum '95% faster'

--- minimum_sum_of_four_digit_number_after_splitting_digits/minimum_sum_of_four_digit_number_after_splitting_digits.js
+++ minimum_sum_of_four_digit_number_after_splitting_digits/minimum_sum_of_four_digit_number_after_splitting_digits.js
@@ -12,8 +12,19 @@ var minimumSum = function(num) {
     return parseInt(num1) + parseInt(num2);
 };
 
+// Faster 95%
+var minimumSum2 = function(nums) {
+    let arr=[];
+    while(nums){
+        arr.push(nums%10);
+        nums = Math.trunc(nums / 10);
+    }//get all the numbers into an array and sort them
+    arr = arr.sort((a,b)=>a-b);
+    return (((arr[0]*10)+arr[3])+((arr[1]*10)+arr[2])) 
+	};
+
 // Example 1
 console.log(minimumSum(2932));
 
 // Example 2
-console.log(minimumSum(4009));
+console.log(minimumSum2(4009));
